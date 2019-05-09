8756a4f912b4ac8031528431fe9757df4bf3845e
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-09 23:02:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   minValue = name_hash.values[0]
   minKey = name_hash.key[0]
-  name_hash.each do |keys , allValue|
+  name_hash.each do |allKeys , allValue|
     if allValue < minValue
-      minKey = keys
+      minKey = allKeys
     end
   return minKey
 end
