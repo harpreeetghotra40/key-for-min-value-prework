e209238e22fbb7a4edce8206521ae724bb1a4e23
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-09 23:01:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  minValue = name_hash.values[0]
+  minKey = name_hash.key[0]
+  name_hash.each do |keys , value|
+    if value < minValue
+      minKey = keys
+    end
+  return minKey
 end
